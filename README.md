252e94d1de5b7a02f7781f52dc5e621611b30326
Silvio Paganini <s2paganini@gmail.com>
2015-06-04 12:18:26 +0100

    update rapid prototype

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 ### Rapid Prototyping boilerplate
 by @mattdesl
 
-@see https://github.com/mattdesl/budo
-
 ```
 git clone 
 npm install 
 npm run start
-http://localhost:9966
 ```
--- demo.js
+++ demo.js
@@ -1,18 +1,9 @@
-var THREE = require('three/three.min');
-var createOrbitViewer = require('three-orbit-viewer')(THREE);
+// your browserify code goes here 
 
-var app = createOrbitViewer({
-    clearColor: 0x000000,
-    clearAlpha: 1.0,
-    fov: 65,
-    position: new THREE.Vector3(1, 1, -2)
-})
- 
-var geo = new THREE.BoxGeometry(1,1,1)
-var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffffff })
-var box = new THREE.Mesh(geo, mat)
-app.scene.add(box)
- 
-app.on('tick', function(dt) {
-    //.. handle pre-render updates     
-})
+/*
+
+var THREE = require('three');
+
+*/
+
+console.log('asdasd')
