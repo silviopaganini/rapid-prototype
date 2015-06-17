1230f91500b8ca92979f825440c7632386fde9c2
Silvio Paganini <s2paganini@gmail.com>
2015-06-17 16:06:53 +0100

    fix last commit

--- demo.js
+++ demo.js
@@ -2,20 +2,6 @@ var THREE         = require('three');
 var OrbitControls = require('three-orbit-controls')(THREE);
 var dat = require('dat-gui');
 
-var audio = new Audio('mp3/lwtua.mp3');
-audio.addEventListener('canplaythrough', init.bind(this));
-
-function init()
-{
-    audio.play()
-    update();
-}
-
-var audioAnalyser = require('web-audio-analyser')(audio);
-audioAnalyser.analyser.fftSize = 2048;
-
-var Line = require('./Line')
-
 var renderer, camera, scene;
 var counter = 0;
 
@@ -31,15 +17,6 @@ controls = new OrbitControls(camera, renderer.domElement);
 controls.maxDistance = 500;
 
 scene = new THREE.Scene();
-scene.fog = new THREE.FogExp2( 0, 0.00255 );
-
-var SIZE = {x: window.innerWidth / 2, y: 30}
-
-var lines = new Array(22)
-for (var i = lines.length - 1; i >= 0; i--) {
-    lines[i] = new Line(-SIZE.y/2 + i, 50, i, SIZE, Math.random() * i >> 0, audioAnalyser.frequencies())
-    scene.add(lines[i].mesh);
-};
 
 function update()
 {
