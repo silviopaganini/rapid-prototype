58099de2843b89a3d59199a9470256447ccb86db
Silvio Paganini <s2paganini@gmail.com>
2015-12-01 13:29:32 +0000

    basic glsl

--- src/glsl/basic_frag.glsl
+++ src/glsl/basic_frag.glsl
@@ -1,4 +1,6 @@
+uniform vec3 color;
+
 void main() {
-    gl_FragColor = vec4( color * vColor, 1.0 );
+    gl_FragColor = vec4( color , 1.0 );
     // gl_FragColor = gl_FragColor * texture2D( texture, vUv );
 }
